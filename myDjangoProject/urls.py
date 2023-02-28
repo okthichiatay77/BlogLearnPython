beb3c5c05c8fcfecab654b0864c2fa4f09daadfc
Phan Anh Tu <okthichiatay77@gmail.com>
2023-02-28 20:29:03 +0700

    Initial commit

---
+++ myDjangoProject/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
+
+urlpatterns = [
+    path('', include('my_app.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
